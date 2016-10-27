f0138c5ddcd77110142b994c71cc9fcaa7e244e2
Sajjad Gul <sajjadarifgul@gmail.com>
2016-10-27 12:33:25 +0500

    Models
    Added Customer & CustomerNumber Model classes in Models Library.

---
+++ MyTask.Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTask.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Customer_Id_Pk { get; set; }
+        public string Customer_Name { get; set; }
+        public string Customer_Details { get; set; }
+        public string Customer_Address { get; set; }
+        public string Customer_Contact { get; set; }
+        public Nullable<int> Customer_Key_Account_Fk { get; set; }
+        public int Created_By { get; set; }
+        public System.DateTime Created_On { get; set; }
+        public Nullable<int> Modified_By { get; set; }
+        public Nullable<System.DateTime> Modified_On { get; set; }
+        public bool Is_Active { get; set; }
+
+        public virtual ICollection<CustomerNumber> CustomerNumbers { get; set; }
+    }
+}
---
+++ MyTask.Models/CustomerNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTask.Models
+{
+    public class CustomerNumber
+    {
+        [Key]
+        public int Customer_Number_Id_Pk { get; set; }
+        public int Customer_Id_Fk { get; set; }
+        public string Customer_Number_Value { get; set; }
+        public string Customer_Number_Details { get; set; }
+        public int Created_By { get; set; }
+        public System.DateTime Created_On { get; set; }
+        public Nullable<int> Modified_By { get; set; }
+        public Nullable<System.DateTime> Modified_On { get; set; }
+        public bool Is_Active { get; set; }
+
+        public virtual Customer Customer { get; set; }
+    }
+}
