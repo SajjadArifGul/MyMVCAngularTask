3f0306927fa3101bc5ced48de313e13e08f966be
Sajjad Gul <sajjadarifgul@gmail.com>
2016-10-27 13:08:18 +0500

    CustomerViewModel
    created Customer View Model in viewmodels folder. I will use this for
    views & sending by API.

---
+++ MyTask.WebUI/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyTask.WebUI.ViewModels
+{
+    public class CustomerViewModel
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public System.DateTime CreatedOn { get; set; }
+        public string ModifiedBy { get; set; }
+        public Nullable<System.DateTime> ModifiedOn { get; set; }
+    }
+}
