41890ed08594a3a60329ed8c3c1c29e5440e70c5
Sajjad Gul <sajjadarifgul@gmail.com>
2016-10-27 12:26:01 +0500

    ASP MVC
    Added ASP .Net MVC Project

---
+++ MyTask.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyTask.WebUI.MvcApplication" Language="C#" %>
---
+++ MyTask.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyTask.WebUI.Startup))]
+namespace MyTask.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
